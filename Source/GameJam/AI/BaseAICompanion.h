c1e6555c6033dab42a0f9180200bf9db154a8439
Alex360221 <46891928+Alex360221@users.noreply.github.com>
2022-07-04 16:54:19 +0100

    Basic AI added

---
+++ Source/GameJam/AI/BaseAICompanion.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseAICharacter.h"
+#include "BaseAICompanion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM_API ABaseAICompanion : public ABaseAICharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseAICompanion();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	bool moveToPlayer = false;
+	bool shouldMove = false;
+
+	bool ShouldStartMove();
+	bool ShouldStopMove();
+
+
+	
+};
